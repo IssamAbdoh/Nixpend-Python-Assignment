d7f3b34f3cda36e4d8404d98a99e2aa6aa15eccc
Issam Abdoh <iss20190134@std.psut.edu.jo>
2022-12-26 22:01:40 +0300

    Add Assignment files

---
+++ Assignment #2/app.py
@@ -0,0 +1,38 @@
+
+from flask import Flask, render_template, request
+import qrcode
+from reportlab.pdfgen import canvas
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def form():
+    return render_template('index.html')
+
+@app.route('/generate_pdf', methods=['POST'])
+def generate_pdf():
+    # Retrieve form data
+    full_name = request.form['full_name']
+    email = request.form['email']
+    phone_number = request.form['phone_number']
+    
+    # Generate QR code
+    data = f"Full name: {full_name}\nEmail: {email}\nPhone number: {phone_number}"
+    qr = qrcode.make(data)
+    
+    # Check if file already exists and delete it
+    file_path = "qr_code.pdf"
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
+    # Create PDF document and add QR code image
+    c = canvas.Canvas("qr_code.pdf")
+    c.drawInlineImage(qr, 0, 0)
+    c.showPage()
+    c.save()
+    
+    with open('success.html', 'r') as f:
+        html_text = f.read()
+
+    return html_text
